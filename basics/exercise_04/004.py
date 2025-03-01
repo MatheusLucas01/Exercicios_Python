afa991947e0c9795ad473e638b8e28e48f9d4998
Matheus <matheuslucasdesouza22@gmail.com>
2025-03-01 14:51:46 -0300

    new exercise and readme

---
+++ basics/exercise_04/004.py
@@ -0,0 +1,26 @@
+
+print("** App impar ou par **")
+while True:
+    try:
+        numero = int(input("Digite um número: "))
+        
+        if numero % 2 == 0:
+            print("Esse número é par.")
+        else:
+            print("Esse número é impar.")
+        
+        while True:
+            pergunta = input("Deseja encerrar o programa? s/n: ").strip().lower()
+
+            if pergunta == "s":
+                print("Obrigado por usar nossos serviços.")
+                exit() # Sai diretamente do sistema.
+            elif pergunta == "n":
+                print("ok continuando...")
+                break
+            else:
+                print("Opção inválida! Digite apenas 's' ou 'n'.")
+
+    except ValueError:
+        print("Digite apenas números inteiros!")
+        
