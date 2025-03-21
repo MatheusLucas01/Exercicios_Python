8d133c436e6f9fff72680326e23b060ac4188a67
Matheus Lucas <matheuslucasdesouza22@gmail.com>
2025-03-21 11:22:01 -0300

    novos exercicios

---
+++ basics/exercise_11/README.md
@@ -0,0 +1,11 @@
+# Fluxo do código
+
+✔ 1. O programa solicita ao usuário que digite um número.
+
+✔ 2. Se o usuário digitar algo que não seja um número, uma mensagem de erro será exibida, pedindo para tentar novamente.
+
+✔ 3. O programa verifica o número inserido:
+
+    Se for maior que 0, exibe a mensagem: O número informado é positivo.
+    Se for menor que 0, exibe a mensagem: O número informado é negativo.
+    Se for igual a 0, exibe a mensagem: None (nenhum número).
---
+++ basics/exercise_12/012.py
@@ -0,0 +1,35 @@
+def caixa_eletronico():
+    while True:
+        try:
+            valor_saque = int(input("Qual valor deseja sacar? "))
+            if valor_saque > 600:
+                print("O valor máximo é R$600")
+            elif valor_saque <= 0:
+                print("O valor deve ser maior que zero!")
+            else:
+                return valor_saque  # Retorna o valor para ser usado na função 'saque'
+        except ValueError:
+            print("Digite apenas números!")
+
+def saque(valor):
+    notas_disponiveis = [200, 100, 50, 20, 10, 5, 2] 
+    resultado = {}
+
+    for nota in notas_disponiveis:
+        if valor >= nota:
+            quantidade = valor // nota
+            valor %= nota
+            resultado[nota] = quantidade  # Guarda quantas notas foram usadas
+
+    return resultado
+
+def main():
+    valor = caixa_eletronico()  # Recebe o valor do usuário
+    notas = saque(valor)  # Chama a função para calcular as notas
+
+    print("\nNotas entregues:")
+    for nota, quantidade in notas.items(): # Percorre o dicionário pegando chave e valor ao mesmo tempo.
+        print(f"{quantidade} nota(s) de {nota} reais")
+
+if __name__ == "__main__":
+    main()
