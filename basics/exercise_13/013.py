a8648875d9740d97e2ebcf63113fc4f3d03a8c22
Matheus <matheuslucasdesouza22@gmail.com>
2025-03-28 16:22:01 -0300

    alterations

--- basics/exercise_13/013.py
+++ basics/exercise_13/013.py
@@ -1,3 +1,10 @@
+def calcular_valor(combustivel, preco, litros):
+    if litros <= 20:
+        desconto = litros * preco * 0.03
+    else:
+        desconto = litros * preco * 0.05
+    valor_total = (litros * preco) - desconto
+    return valor_total, desconto
 
 def calcular_abastecimento():
     preco_gasolina = 2.5
@@ -6,49 +13,22 @@ def calcular_abastecimento():
     while True:
         try:
             escolha = int(input("Escolha uma opção: 1 - Gasolina, 2 - Alcool: "))
-
             if escolha == 1:
-                gasolina = float(input("Quantos litros de gasolina? "))
-                if gasolina <= 20:
-                    desconto = gasolina * preco_gasolina * 0.03
-                else:
-                    desconto = gasolina * preco_gasolina * 0.05
-                valor_gasolina = (gasolina * preco_gasolina) - desconto
-                print(f"\nValor da gasolina: R${valor_gasolina:.2f}")
+                litros = float(input("Quantos litros de gasolina? "))
+                if litros <= 0:
+                    print("Os valores devem ser positivos.")
+                    continue
+                valor_gasolina, desconto = calcular_valor('Gasolina', preco_gasolina, litros)
+                print(f"Valor total da gasolina com desconto: R${valor_gasolina:.2f}")
                 break
             elif escolha == 2:
-                alcool = float(input("Quantos litros de álcool? "))
-                # Calculando o desconto do álcool
-                if alcool <= 20:
-                    desconto = alcool * preco_alcool * 0.03  # 3% de desconto sobre o valor total
-                else:
-                    desconto = alcool * preco_alcool * 0.05  # 5% de desconto sobre o valor total
-                valor_alcool = (alcool * preco_alcool) - desconto
+                litros = float(input("Quantos litros de álcool? "))
+                if litros <= 0:
+                    print("Os valores devem ser positivos.")
+                    continue
+                valor_alcool, desconto = calcular_valor('Álcool', preco_alcool, litros)
                 print(f"Valor do álcool com desconto: R${valor_alcool:.2f}")
-
-
-
-
-            
-
-            if gasolina < 0 or alcool < 0:
-                print("Os valores devem ser positivos.")
-                continue
-
-           
-
-            
-
-            
-
-            total_pagar = valor_gasolina + valor_alcool
-
-            
-            
-            print(f"Desconto aplicado no álcool: R${desconto:.2f}")
-            print(f"Total a pagar: R${total_pagar:.2f}")
-
-            break  # Sai do loop ao finalizar o cálculo
+                break
         except ValueError:
             print("Digite apenas números!")
 
